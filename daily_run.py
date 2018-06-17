0851e241f35c14f22ffc80926aa58fe3e76a74ab
andy566159@gmail.comgit config --global user.email andy566159@gmail.com <andy566159@gmail.com>
2018-06-18 00:59:42 +0800

    add new user to receivers email list

--- daily_run.py
+++ daily_run.py
@@ -4,7 +4,7 @@ from OTC import *
 start = datetime.now()
 
 #main
-receivers_email_list = ["andy566159@gmail.com", "gn02827186@gmail.com"]
+receivers_email_list = ["andy566159@gmail.com", "gn02827186@gmail.com", "s9101836@gmail.com"]
 gmail(receivers_email_list, OTC_deal_with_data()).send_gmail()
 gmail(receivers_email_list, Exchange_deal_with_data()).send_gmail()
 
